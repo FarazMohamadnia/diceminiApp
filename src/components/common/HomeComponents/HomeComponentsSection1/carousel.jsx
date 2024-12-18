6825742f57b2b19f436d1592086f14d76a288756
farazmn <farazmnm@gmail.com>
2024-12-18 15:18:12 +0330

    fix : Add animation Icon to Homepage carousel

--- src/components/common/HomeComponents/HomeComponentsSection1/carousel.jsx
+++ src/components/common/HomeComponents/HomeComponentsSection1/carousel.jsx
@@ -4,11 +4,19 @@ import "./carousel.css";
 import img from "../../../../asset/img/HomeImg/carouselImage/image1.png";
 import img1 from "../../../../asset/img/HomeImg/carouselImage/image2.png";
 import img2 from "../../../../asset/img/HomeImg/carouselImage/image3.png";
-
+import gif from '../../../../asset/Gif/ArrowTop.gif'
 const images = [img, img1, img2];
 
 export default function Carousel1() {
   const [index, setIndex] = useState(0);
+  const [Icons , setIcons]=useState({
+    Icon1 : true ,
+    Icon2 : true
+  })
+
+  const IconHandller = (e)=>{
+    console.log(e)
+  }
 
   const nextImage = () => {
     setIndex((prev) => (prev + 1) % images.length);
@@ -19,13 +27,17 @@ export default function Carousel1() {
   };
 
   return (
-    <div className="text-center mt-2">
+    <div className="relative text-center mt-2">
+        <div>
+          <img className={Icons.Icon1 ? 'absolute right-[40%] top-3 z-10 w-16 h-16' : 'hidden'} src={gif} alt="" />
+        </div>
       <div className="Carousel1-Container">
         {images.map((image, i) => {
           const positionIndex = (i - index + images.length) % images.length;
 
           return (
             <motion.div
+              onClick={IconHandller}
               key={i}
               className="Carousel1-Image-card-Style"
               style={{
@@ -37,14 +49,25 @@ export default function Carousel1() {
               dragConstraints={{ top: 0, bottom: 0 }}
               onDragEnd={(event, info) => {
                 if (info.offset.y > 100) {
+                  setIcons({
+                    Icon1 : false, 
+                    Icon2 : true
+                  })
                   prevImage();
                 } else if (info.offset.y < -100) {
                   nextImage();
+                  setIcons({
+                    Icon1 : true, 
+                    Icon2 : false
+                  })
                 }
               }}
             />
           );
         })}
+      </div>
+      <div>
+          <img className={Icons.Icon2 ? `transform rotate-180 right-[40%] z-10 bottom-3 absolute w-16 h-16` : 'hidden'} src={gif} alt="" />
       </div>
     </div>
   );
