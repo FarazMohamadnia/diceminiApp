45dc2f749a17ee4b0aba1e6f593b5577f7cc029b
farazmn <farazmnm@gmail.com>
2025-01-12 12:20:09 +0330

    feat : changed dice page

---
+++ src/components/icons/swapicon.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+const SwapIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    fill="none"
+    viewBox="0 0 26 26"
+  >
+    <g id="vuesax/linear/arrange-circle">
+      <g id="arrange-circle" strokeLinecap="round" strokeLinejoin="round">
+        <g id="Group" stroke="#1AE5A1" strokeMiterlimit="10" strokeWidth="1.5">
+          <path id="Vector" d="m14.972 7.42 3.293 3.294"></path>
+          <path id="Vector_2" d="M14.972 18.58V7.42"></path>
+        </g>
+        <g
+          id="Group_2"
+          stroke="#1AE5A1"
+          strokeMiterlimit="10"
+          strokeWidth="1.5"
+        >
+          <path id="Vector_3" d="m11.028 18.58-3.293-3.294"></path>
+          <path id="Vector_4" d="M11.028 7.42v11.16"></path>
+        </g>
+        <path
+          id="Vector_5"
+          stroke="#fff"
+          strokeOpacity="0.37"
+          strokeWidth="0.5"
+          d="M13 23.833c5.983 0 10.833-4.85 10.833-10.833S18.983 2.167 13 2.167 2.167 7.017 2.167 13 7.016 23.833 13 23.833"
+        ></path>
+      </g>
+    </g>
+  </svg>
+);
+
+export default SwapIcon;
