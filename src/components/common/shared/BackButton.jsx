57baec399f3a4640e4871f6902173edfacdc0116
farazmn <farazmnm@gmail.com>
2025-01-15 11:59:18 +0330

    fix : backbutton update for change color

--- src/components/common/shared/BackButton.jsx
+++ src/components/common/shared/BackButton.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import BackIcon from "../../icons/back";
 import {useNavigate} from 'react-router-dom'
+import ArrowLeft from "../../icons/arrowleft";
 
-const BackButton = ({ title }) => {
+const BackButton = ({ title , color }) => {
 
   const navigate  = useNavigate()
 
   return (
     <div className="flex items-center cursor-pointer mt-2" onClick={()=>navigate(-1)}>
       <span className="mt-0.5">
-        <BackIcon />
+        {
+          color ? <ArrowLeft /> :<BackIcon />
+        }
       </span>{" "}
-      <span className="ml-1 text-[#CAFD7B] text-sm">{title}</span>
+      <span className={`ml-1 ${color ? 'text-white' :  'text-[#CAFD7B]'} text-sm`}>{title}</span>
     </div>
   );
 };
---
+++ src/components/icons/arrowleft.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+const ArrowLeft = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="11"
+    height="11"
+    fill="none"
+    viewBox="0 0 11 11"
+  >
+    <g id="vuesax/linear/arrow-left">
+      <g id="arrow-left">
+        <path
+          id="Vector"
+          stroke="#fff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          d="M6.875 9.13 3.887 6.142a.91.91 0 0 1 0-1.284L6.875 1.87"
+        ></path>
+      </g>
+    </g>
+  </svg>
+);
+
+export default ArrowLeft;
