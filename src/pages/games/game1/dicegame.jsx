2a03097d7ea3c14b03b92db0451aa6b4f2d21996
farazmn <farazmnm@gmail.com>
2025-01-15 12:02:50 +0330

    feat : add dts number and botton to game1 and create game1 V1

--- src/pages/games/game1/dicegame.jsx
+++ src/pages/games/game1/dicegame.jsx
@@ -4,6 +4,9 @@ import '../index.css'
 import Controller from './components/controller'
 import Table from './components/table'
 import RollBtn from './asset/rollbtn';
+import GreenBtn from './asset/greenbtn';
+import WhiteBtn from './asset/whitebtn';
+import BackButton from '../../../components/common/shared/BackButton';
 
 
 export default function DiceGame1(){
@@ -25,7 +28,9 @@ export default function DiceGame1(){
     }
     return(
         <div className='DiceGame1-container'>
-            <div></div>
+            <div className='mx-7 my-5'>
+                <BackButton color={true} title={'Back To Home'}/>
+            </div>
             <div>
                 <Table />
             </div>
@@ -45,6 +50,41 @@ export default function DiceGame1(){
                     <RollBtn />
                     <span onClick={rollHnadler} className='w-full h-full absolute top-0 rounded-full'></span>
                     <p className='text-white text-[20px] mt-1 text-center font-normal'>Roll</p>
+                </div>
+            </div>
+            <div className='flex justify-between items-center mx-3 mt-8'>
+                <div className='flex'>
+                    <div className='flex justify-center items-center relative'>
+                        <WhiteBtn />
+                        <p className='absolute text-black text-xs font-extrabold'>
+                            Min
+                        </p>
+                    </div>
+                    <div className='flex justify-center items-center relative mx-1'>
+                        <GreenBtn />
+                        <p className='absolute'>
+                            <svg width="17" height="8" viewBox="0 0 17 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="0.850098" y="0.950012" width="15.3" height="6.375" rx="3.1875" fill="white" />
+                            </svg>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <p className='text-white text-[15px] font-bold'>2.00<span className='text-[#1ae5a1] ms-1'>DTS</span></p>
+                </div>
+                <div className='flex'>
+                    <div className='flex justify-center items-center relative mx-1'>
+                        <GreenBtn />
+                        <p className='absolute'>
+                            2
+                        </p>
+                    </div>
+                    <div className='flex justify-center items-center relative'>
+                        <WhiteBtn />
+                        <p className='absolute text-black text-xs font-extrabold'>
+                            Max
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
