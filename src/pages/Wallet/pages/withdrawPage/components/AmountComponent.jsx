1e6977b5df34668e3a8f2cd135d20f69b7b756e6
farazmn <farazmnm@gmail.com>
2025-01-20 10:08:51 +0330

    fix : amount input activated

--- src/pages/Wallet/pages/withdrawPage/components/AmountComponent.jsx
+++ src/pages/Wallet/pages/withdrawPage/components/AmountComponent.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react'
 import './index.css'
 export default function Amount(){
+    const [amount , setamount]=useState(0)
     return(
-        <div>
-            <div className='AmountComponent-inputs-style'>
-                <div><p className='text-[#3bffff] text-[15px] font-light'>AMOUNT</p></div>
-                <div>
-                    <p className='text-white/70 text-[15px] font-light h-6 w-11 px-1 py-[3px] bg-[#1ae5a1]/2 rounded-[7px] border border-[#1ae5a1] justify-center items-center gap-2.5 inline-flex mr-1'>%50</p>
-                    <p className='text-white/70 text-[15px] font-light h-6 w-11 px-1 py-[3px] bg-[#1ae5a1]/2 rounded-[7px] border border-[#1ae5a1] justify-center items-center gap-2.5 inline-flex mr-1'>%100</p>
+        <div className='relative'>
+                <input onChange={(e)=> setamount(e.target.value)} placeholder='1000 USDT' className='AmountComponent-inputs-style' type='number'/>
+                <p className='text-[#3bffff] text-[15px] font-light absolute top-[15px] left-5'>USD AMOUNT:</p>
+                <div className='absolute top-[12px] right-2'>
+                    <button className='text-white/70 text-[15px] font-light h-6 w-11 px-1 py-[3px] bg-[#1ae5a1]/2 rounded-[7px] border border-[#1ae5a1] justify-center items-center gap-2.5 inline-flex mr-1'>%50</button>
+                    <button className='text-white/70 text-[15px] font-light h-6 w-11 px-1 py-[3px] bg-[#1ae5a1]/2 rounded-[7px] border border-[#1ae5a1] justify-center items-center gap-2.5 inline-flex mr-1'>%100</button>
                 </div>
+           
             </div>
-        </div>
     )
 }
