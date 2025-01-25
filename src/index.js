cb8e5d3f6851eb56798bd6bb46cae20999640fdd
mamad <you@example.com>
2025-01-25 10:48:34 +0330

    clean code with alt + ctrl + l

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DicePage from "./pages/Dice/dicePage";
 import ProfilePage from "./pages/Profile/profilePage";
-import { RouteProvider } from "./context/BottomLinkContext/bottomlinkcontext";
+import {RouteProvider} from "./context/BottomLinkContext/bottomlinkcontext";
 import DetailDice from "./pages/DetailDice";
 import DetailDiceUser from "./pages/DetailUserDices";
 import Wallet from "./pages/Wallet";
@@ -16,37 +16,37 @@ import DiceHistoriPage from "./pages/DiceHistori";
 import LeaderBoard from "./pages/Game/leaderboard";
 import GamePage from "./pages/Game/gamePage";
 import Notification from "./pages/notificationpage/notification";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import {TonConnectUIProvider} from "@tonconnect/ui-react";
 import DiceGame1 from "./pages/games/game1/dicegame";
 import DiceGame2 from "./pages/games/game2/dicegame2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-  <div className="overflow-x-hidden max-w-lg">
-    <BrowserRouter>
-      <RouteProvider>
-        <TonConnectUIProvider manifestUrl="http://localhost:3000/maniest-example.json">
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/Dice" element={<DicePage />} />
-            <Route path="/dice/histori" element={<DiceHistoriPage />} />
-            <Route path="/dice-detail" element={<DetailDice />} />
-            <Route path="/dice-detail-user" element={<DetailDiceUser />} />
-            <Route path="/wallet/sell" element={<Dicesellpages />} />
-            <Route path="/games" element={<GamePage />} />
-            <Route path="/games/leaderboard" element={<LeaderBoard />} />
-            <Route path="/Wallet" element={<Wallet />} />
-            <Route path="/Wallet/withdraw" element={<WithdrawPage />} />
-            <Route path="/Wallet/deposite" element={<Deposite />} />
-            <Route path="/Profile" element={<ProfilePage />} />
-            <Route path="/notification" element={<Notification />} />
-            <Route path="/games/game1" element={<DiceGame1 />} />
-            <Route path="/games/game2" element={<DiceGame2 />} />
-          </Routes>
-        </TonConnectUIProvider>
-      </RouteProvider>
-    </BrowserRouter>
-    </div>
-  </React.StrictMode>
+    <React.StrictMode>
+        <div className="overflow-x-hidden max-w-lg">
+            <BrowserRouter>
+                <RouteProvider>
+                    <TonConnectUIProvider manifestUrl="http://localhost:3000/maniest-example.json">
+                        <Routes>
+                            <Route path="/" element={<App/>}/>
+                            <Route path="/Dice" element={<DicePage/>}/>
+                            <Route path="/dice/histori" element={<DiceHistoriPage/>}/>
+                            <Route path="/dice-detail" element={<DetailDice/>}/>
+                            <Route path="/dice-detail-user" element={<DetailDiceUser/>}/>
+                            <Route path="/wallet/sell" element={<Dicesellpages/>}/>
+                            <Route path="/games" element={<GamePage/>}/>
+                            <Route path="/games/leaderboard" element={<LeaderBoard/>}/>
+                            <Route path="/Wallet" element={<Wallet/>}/>
+                            <Route path="/Wallet/withdraw" element={<WithdrawPage/>}/>
+                            <Route path="/Wallet/deposite" element={<Deposite/>}/>
+                            <Route path="/Profile" element={<ProfilePage/>}/>
+                            <Route path="/notification" element={<Notification/>}/>
+                            <Route path="/games/game1" element={<DiceGame1/>}/>
+                            <Route path="/games/game2" element={<DiceGame2/>}/>
+                        </Routes>
+                    </TonConnectUIProvider>
+                </RouteProvider>
+            </BrowserRouter>
+        </div>
+    </React.StrictMode>
 );
