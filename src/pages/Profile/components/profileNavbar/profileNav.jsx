fac5c3f924c87913528fa7b61e502ed4ba708f02
mamad <you@example.com>
2025-01-26 16:25:58 +0330

    feat : all new logo to navbar of the profileNav.jsx

--- src/pages/Profile/components/profileNavbar/profileNav.jsx
+++ src/pages/Profile/components/profileNavbar/profileNav.jsx
@@ -4,31 +4,35 @@ import img from '../../../../asset/img/NavbarImg/PIC.png'
 import Layer1 from '../../../../components/icons/change/profileNavbar/layer1'
 import ProfileShadow from '../../../../components/icons/change/profileNavbar/profileshadow'
 import BackgroundImg from '../../../../components/icons/change/profileNavbar/background'
-export default function ProfileNav(){
-    return(
+import BigLogo from "../../../../components/icons/BigLogo";
+
+export default function ProfileNav() {
+    return (
         <div className=''>
-            <div className='mt-10 mb-4'>
-                Icon
+            <div className='my-10 flex justify-center '>
+                <BigLogo/>
             </div>
             <div className='w-[315px] h-[70px] mx-auto relative'>
-                <div className='absolute right-9 top-[-3px]'><ProfileShadow /></div>
+                <div className='absolute right-9 top-[-3px]'><ProfileShadow/></div>
                 <div className='absolute'>
-                    <BackgroundImg />
+                    <BackgroundImg/>
                 </div>
                 <div className='relative w-[145px] h-[145px] top-[-50px] left-[-45PX]'>
                     <div className='absolute'>
-                        <BorderShadow />
+                        <BorderShadow/>
                         <img className='absolute rounded-[50%] w-[75px] h-[71px] top-[25%] right-[26%]' src={img}/>
                     </div>
                 </div>
-                <div className='profileNav-polygon-shape'>   
-                      <p>faraz</p>
+                <div className='profileNav-polygon-shape'>
+                    <p>faraz</p>
                 </div>
                 <div className='absolute top-[5px] right-10'>
-                    <p className='flex'><span className='mr-[6px]'><Layer1 /></span> <span className='text-[#1ae5a1] text-xl font-bold'>level 1</span></p>
+                    <p className='flex'><span className='mr-[6px]'><Layer1/></span> <span
+                        className='text-[#1ae5a1] text-xl font-bold'>level 1</span></p>
                 </div>
                 <div className='absolute right-7 bottom-[18px] flex items-center'>
-                    <p className='profileNav-progress'><span className='w-[70%] h-[98%] bg-[#1AE5A1] absolute rounded-[45px]'></span></p>
+                    <p className='profileNav-progress'><span
+                        className='w-[70%] h-[98%] bg-[#1AE5A1] absolute rounded-[45px]'></span></p>
                     <p className='text-[#1ae5a1] text-[8px] font-normal'><span>3000/5500</span>XP</p>
                 </div>
             </div>
