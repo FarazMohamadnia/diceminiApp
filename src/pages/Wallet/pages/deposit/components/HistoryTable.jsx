4d608249b35c73bdab76700d35469b2e433be473
farazmn <farazmnm@gmail.com>
2025-02-15 13:08:25 +0330

    feat : add dynamic deposit table

--- src/pages/Wallet/pages/deposit/components/HistoryTable.jsx
+++ src/pages/Wallet/pages/deposit/components/HistoryTable.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { SucessFullIcon } from "../../../../../components/icons/successfull";
 import { PendingIcon } from "../../../../../components/icons/pending";
 import CalenderIcon from "../../../../../components/icons/calender";
+import CancelledIcon from "../../../../../components/icons/cancelled";
+import useTokenStore from "../../../../../store/token";
+import { Api } from "../../../../../api/apiUrl";
+import axios from "axios";
 
 const transactions = [
   {
@@ -56,6 +60,64 @@ const transactions = [
 ];
 
 const HistoryTable = () => {
+    const [transitionData ,settransitionData]=useState([]);
+    const [renderHandler , setrenderHandler]=useState(true);
+    const{token}=useTokenStore();
+    const iconMap = {
+      1: <SucessFullIcon />,
+      2: <PendingIcon />,
+      3: <CancelledIcon />
+    };
+    const statusMap = {
+      1: 'Successful',
+      2: 'Processing',
+      3: 'Cancelled'
+    };
+    const statusColorMap = {
+      1: "#1AE5A1",
+      2: "#FFCF60",
+      3: "#FF4D4D" 
+    };
+    const signMap = {
+      1: "+",
+      2: "-"
+  };
+  
+    const tabledata = async()=>{
+      try{
+        const response = await axios.get(Api[4].depositsTable ,{
+          headers:{
+             "Authorization" : `token ${token}`
+          }
+        })
+        if(renderHandler){ 
+          response.data.map(data =>{
+            const date = new Date(data.insert_dt);
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1
+            const day = date.getDay()
+            setrenderHandler(false)
+            console.log(data)
+            const obg={
+              status :statusMap[data.status] ,
+              icon: iconMap[data.status],
+              statusColor: statusColorMap[data.status],
+              amount: `${signMap[data.transaction_type]}${data.ton_amount.toFixed(2)} TON`,
+              amountColor : statusColorMap[data.status] ,
+              date : `${year}/${month}/${day}`
+            }
+            transitionData.push(obg)
+          })   
+        }
+        console.log(transitionData)   
+      }catch(err){
+        console.log(err)
+      }
+    }
+  
+    useEffect(()=>{
+      tabledata()
+    },[transitionData])
   return (
     <div
       className="
@@ -66,6 +128,8 @@ const HistoryTable = () => {
     overflow-hidden
     linear-gradient-blue-table
     backdrop-blur-[14.4px]
+    overflow-y-scroll ,
+    h-[200px]
   "
     >
       {" "}
@@ -77,7 +141,7 @@ const HistoryTable = () => {
         </div>
       </div>
       <div className="mt-8 flex flex-col divide-y divide-[#cafd7b3b]">
-        {transactions.map((tx, index) => (
+        {transitionData.map((tx, index) => (
           <div
             key={index}
             className="flex items-center justify-between py-3 px-2"
