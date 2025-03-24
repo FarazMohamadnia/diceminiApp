8ee4eb1edc349f4f41f19912059aa7eb6b3cdeab
farazmn <farazmnm@gmail.com>
2025-03-24 12:34:03 +0330

    fix : fix token bug

--- src/store/token.jsx
+++ src/store/token.jsx
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 const useTokenStore = create((set) => ({
-    token: "d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35", 
+    token: "", 
     settoken: (newtoken) => set({ token: newtoken }), 
 }));
 
